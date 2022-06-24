53e7377db698d791676ce21370f37fd249f195c3
Christophe PARISEL <94186254+labyrinthinesecurity@users.noreply.github.com>
2022-06-24 09:24:05 +0000

    Update quantumWalk6nodes.py

--- quantumWalk6nodes.py
+++ quantumWalk6nodes.py
@@ -8,11 +8,9 @@ from scipy.optimize import minimize, basinhopping, shgo, dual_annealing
 from scipy.integrate import odeint, solve_ivp
 from scipy.linalg import sqrtm
 
-#useful global variables, shouldn't be too inefficient
 global dimension
 global H
 
-#routine to generate loop hamiltonian + oracle state
 def generate_hamiltonian():
 
     laplacian = np.empty([dimension, dimension],dtype=complex)
@@ -67,30 +65,12 @@ def solve_schrodinger_equation(time):
         psi_t[i] = sh_solved.y[i, len(sh_solved.y[i])-1]
 
     normalization = np.dot(np.conj(psi_t), psi_t)
-    print(st,'Normalization:',normalization.real,psi_t)
-    print(st,linalg.norm(psi_t[0]),linalg.norm(psi_t[1]),linalg.norm(psi_t[2]))
+    print(str(st)+";"+str(linalg.norm(psi_t[0]))+";"+str(linalg.norm(psi_t[1]))+";"+str(linalg.norm(psi_t[2]))+";"+str(linalg.norm(psi_t[3]))+";"+str(linalg.norm(psi_t[4]))+";"+str(linalg.norm(psi_t[5])))
     return psi_t,normalization.real
-    #return normalization.real
 
-
-#zz=np.empty([3,3])
-#zz.fill(0)
-#zz[0,0]=2
-#zz[0,2]=1
-#zz[1,1]=2
-#zz[1,2]=1
-#zz[2,0]=1
-#zz[2,1]=1
-#zz[2,2]=2
-#print(zz)
-#nu=sqrtm(zz)
-#nuInv=linalg.inv(nu)
-#print(nu)
-#print(nuInv)
 dimension = 6
 H=generate_hamiltonian()
 print('H')
-#print(H)
 H=H.transpose()
 print(H)
 evals,evecs=linalg.eig(H)
@@ -100,106 +80,27 @@ print(evecs)
 i=0
 zz=np.empty([dimension,dimension],dtype=np.double)
 zz.fill(0)
-phi1=np.empty([dimension])
-phi1.fill(0)
-phi1[0]=evecs[0][0]
-phi1[1]=evecs[1][0]
-phi1[2]=evecs[2][0]
-phi1[3]=evecs[3][0]
-phi1[4]=evecs[4][0]
-phi1[5]=evecs[5][0]
-phi2=np.empty([dimension])
-phi2.fill(0)
-phi2[0]=evecs[0][1]
-phi2[1]=evecs[1][1]
-phi2[2]=evecs[2][1]
-phi2[3]=evecs[3][1]
-phi2[4]=evecs[4][1]
-phi2[5]=evecs[5][1]
-phi3=np.empty([dimension])
-phi3.fill(0)
-phi3[0]=evecs[0][2]
-phi3[1]=evecs[1][2]
-phi3[2]=evecs[2][2]
-phi3[3]=evecs[3][2]
-phi3[4]=evecs[4][2]
-phi3[5]=evecs[5][2]
-
-phi4=np.empty([dimension])
-phi4.fill(0)
-phi4[0]=evecs[0][3]
-phi4[1]=evecs[1][3]
-phi4[2]=evecs[2][3]
-phi4[3]=evecs[3][3]
-phi4[4]=evecs[4][3]
-phi4[5]=evecs[5][3]
-phi5=np.empty([dimension])
-phi5.fill(0)
-phi5[0]=evecs[0][4]
-phi5[1]=evecs[1][4]
-phi5[2]=evecs[2][4]
-phi5[3]=evecs[3][4]
-phi5[4]=evecs[4][4]
-phi5[5]=evecs[5][4]
-phi6=np.empty([dimension])
-phi6.fill(0)
-phi6[0]=evecs[0][5]
-phi6[1]=evecs[1][5]
-phi6[2]=evecs[2][5]
-phi6[3]=evecs[3][5]
-phi6[4]=evecs[4][5]
-phi6[5]=evecs[5][5]
-
-print(phi1)
-print(phi2)
-print(phi3)
-print(phi4)
-print(phi5)
-print(phi6)
-
-
-
-if np.amin(phi3)!=0:
-  phi3=phi3/np.amax(phi3)
-if np.amin(phi2)!=0:
-  phi2=phi2/np.amax(phi2)
-if np.amin(phi1)!=0:
-  phi1=phi1/np.amax(phi1)
-
-if np.amin(phi4)!=0:
-  phi4=phi4/np.amax(phi4)
-if np.amin(phi5)!=0:
-  phi5=phi5/np.amax(phi5)
-if np.amin(phi6)!=0:
-  phi6=phi6/np.amax(phi6)
-
-
+phi=[]
+for aD in range(0,dimension):
+  aPhi=np.empty([dimension])
+  aPhi.fill(0)
+  aPhi[0]=evecs[0][aD]
+  aPhi[1]=evecs[1][aD]
+  aPhi[2]=evecs[2][aD]
+  aPhi[3]=evecs[3][aD]
+  aPhi[4]=evecs[4][aD]
+  aPhi[5]=evecs[5][aD]
+  phi.append(aPhi)
+  zz=zz+np.outer(aPhi,aPhi)
 print('PHI')
-print(phi1)
-print(phi2)
-print(phi3)
-print('PHI',phi4,phi5,phi6)
-zz=zz+np.outer(phi1,phi1)
-zz=zz+np.outer(phi2,phi2)
-zz=zz+np.outer(phi3,phi3)
-
-zz=zz+np.outer(phi4,phi4)
-zz=zz+np.outer(phi5,phi5)
-zz=zz+np.outer(phi6,phi6)
+for ap in phi:
+  print("..",ap)
 print(zz)
 nu=sqrtm(zz)
 nuInv=linalg.inv(nu)
 H=H.transpose()
 H=nu.dot(H.dot(nuInv))
 print(H)
-normMin=999999999999999999999999999999999999.0
-normMax=0.0
-for i in range(0,80):
-  st=float(i)/float(20)
+for i in range(0,160):
+  st=float(i)/float(5)
   rez,norm=solve_schrodinger_equation(st)
-#  print("..",norm,normMin,normMax)
-  if norm<normMin:
-    normMin=norm
-  if norm>normMax:
-    normMax=norm
-#print("delta",normMin,normMax,abs(normMin-normMax))
